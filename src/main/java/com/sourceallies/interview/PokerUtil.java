ad1208035ad2ad7b490e2eba1a28aff0d37f62fa
athoreso <athoreso@iastate.edu>
2025-03-07 01:37:51 +0000

    Outlining logic for hand comparision

--- src/main/java/com/sourceallies/interview/PokerUtil.java
+++ src/main/java/com/sourceallies/interview/PokerUtil.java
@@ -18,6 +18,17 @@ public class PokerUtil {
     }
 
     static public PokerHand compareHands(PokerHand white, PokerHand black) {
+        // what is required to compare hands?
+
+        // cards are compared based on two things
+        // 1. Rank
+        // 2. HighCard of a given rank
+        // ex. if two of a kind, the highest value of the pair wins
+        // ex. if full house, broken by highest 3 of a kind
+
+        // Observation
+        // Cards should have knowlege of the highest value/tie break value in each case
+
         return white;
     }
 
