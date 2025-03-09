edb3881c802c1071600e20cd1ba932c690e6ae4f
athoreso <athoreso@iastate.edu>
2025-03-09 16:17:20 +0000

    updating poker rank enum to have corresponding int values for rank

--- src/main/java/com/sourceallies/interview/PokerRank.java
+++ src/main/java/com/sourceallies/interview/PokerRank.java
@@ -1,13 +1,24 @@
 package com.sourceallies.interview;
 
-public enum PokerRank{
-    HIGHCARD,
-    PAIR,
-    TWOPAIRS,
-    THREEOFAKIND,
-    FLUSH,
-    STRAIGHT,
-    FULLHOUSE,
-    FOUROFAKIND,
-    STRAIGHTFLUSH
+public enum PokerRank {
+    HIGHCARD(0),
+    PAIR(1),
+    TWOPAIRS(2),
+    THREEOFAKIND(3),
+    FLUSH(4),
+    STRAIGHT(5),
+    FULLHOUSE(6),
+    FOUROFAKIND(7),
+    STRAIGHTFLUSH(8);
+
+    private final int rank;
+
+    PokerRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getInt() {
+        return rank;
+    }
+
 }
