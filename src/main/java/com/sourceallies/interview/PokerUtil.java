e2e7ac8209d1c0b118018cacab1d2036dc0440fd
athoreso <18athoreso@gmail.com>
2025-03-09 16:21:23 +0000

    outlining logic for hand comparisons

--- src/main/java/com/sourceallies/interview/PokerUtil.java
+++ src/main/java/com/sourceallies/interview/PokerUtil.java
@@ -27,9 +27,42 @@ public class PokerUtil {
         // ex. if full house, broken by highest 3 of a kind
 
         // Observation
-        // Cards should have knowlege of the highest value/tie break value in each case
+        // Poker hands should have knowlege of the highest value/tie break value in each
+        // case
+
+        // hands have different ranks, find a way to comapre them
+        // may want to do this with a comparable interface
+
+        // if hands have same rank, need to tiebreak
+        if (white.getRank() == black.getRank()) {
+            // compare high cards
+            if (white.getTieBreakCard().compareTo(black.getTieBreakCard()) == 0) {
+                //
+                System.out.println("Tie game!");
+            } else if (white.getTieBreakCard().compareTo(black.getTieBreakCard()) == -1) {
+                // white wins
+                System.out.println("White wins - " + white.getRank());
+            } else {
+                // black wins
+                System.out.println("Black wins - " + black.getRank() + " ");
+            }
+        }
 
         return white;
     }
+
+    // Output Examples
+    // Examples:
+    // Input: Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH
+    // Output: White wins - high card: Ace
+
+    // Input: Black: 2H 4S 4C 3D 4H White: 2S 8S AS QS 3S
+    // Output: White wins - flush
+
+    // Input: Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C KH
+    // Output: Black wins - high card: 9
+
+    // Input: Black: 2H 3D 5S 9C KD White: 2D 3H 5C 9S KH
+    // Output: Tie
 
 }
